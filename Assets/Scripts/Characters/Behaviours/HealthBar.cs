fb14ccae50ed78a777061693b019fc39db525271
Renat <renat_samigullin_97@mail.ru>
2024-03-20 02:53:41 +0300

    Визуализация изменения HealthBar при получении урона

--- Assets/Scripts/Characters/Behaviours/HealthBar.cs
+++ Assets/Scripts/Characters/Behaviours/HealthBar.cs
@@ -1,10 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Characters.Behaviours
 {
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] private Camera _camera;
+        [SerializeField] private Image _healthBarFill;
+        [SerializeField] private Character _character;
+
+        private void Awake()
+        {
+            _character.HealthChanged += OnHealthChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _character.HealthChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged(float valuesAsPercantage)
+        {
+            _healthBarFill.fillAmount = valuesAsPercantage;
+        }
 
         private void LateUpdate()
         {
